34aef823cfca7830fa4e726611ae5d7751b23262
Alexandr <86582717+tseneven@users.noreply.github.com>
2025-04-28 13:52:33 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace prKol_ind2_Кузнецов_а_Пр_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("txt.txt"))
+            {
+                Queue<string[]> famale = new Queue<string[]>();
+                Queue<string[]> male = new Queue<string[]>();
+                List<string[]> persons = new List<string[]>();
+                StreamReader sr = new StreamReader("txt.txt");
+                while (!sr.EndOfStream)
+                {
+                    string[] person = new string[4];
+                    person[0] = sr.ReadLine();
+                    person[1] = sr.ReadLine();
+                    person[2] = sr.ReadLine();
+                    person[3] = sr.ReadLine();
+                    if (person[1] == "М")
+                    {
+                        male.Enqueue(person);
+                    }
+                    else if (person[1] == "Ж")
+                    {
+                        famale.Enqueue(person);
+                    }
+                }
+                sr.Close();
+
+                while (male.Count > 0)
+                {
+                    persons.Add(male.Dequeue());
+                }
+                while (famale.Count > 0)
+                {
+                    persons.Add(famale.Dequeue());
+                }
+                foreach (var p in persons)
+                {
+                    Console.WriteLine(p[0]);
+                    Console.WriteLine(p[1]);
+                    Console.WriteLine(p[2]);
+                    Console.WriteLine(p[3]);
+                    Console.WriteLine("Нажмите Enter для следующей персоны");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такого файла нету");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("prKol_ind2_Кузнецов_а_Пр-23")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("prKol_ind2_Кузнецов_а_Пр-23")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f78e9c89-897d-411f-809b-504b655e8d43")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
