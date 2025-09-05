5a8197a9cbffdc51b7075111fa9b53b291a9bcc3
Alexandr <86582717+tseneven@users.noreply.github.com>
2025-09-05 13:13:10 +0500

    fix

--- app/src/main/java/com/example/pr_33_kuznetsov_v/CreditCalculatorActivity.kt
+++ app/src/main/java/com/example/pr_33_kuznetsov_v/CreditCalculatorActivity.kt
@@ -1,11 +1,23 @@
 package com.example.pr_33_kuznetsov_v
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CreditCalculatorActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.credit_calculator_activity)
+        val backButton : Button = findViewById(R.id.button_back)
+
+        backButton.setOnClickListener{
+            back()
+        }
+    }
+
+    fun back(){
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
