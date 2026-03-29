572b45f227e783582f5673323c853c93fdac0ff2
Alexandr <86582717+tseneven@users.noreply.github.com>
2026-03-30 04:07:14 +0500

    +

---
+++ Prakt15Client/Prakt15Client/Prakt15Client.cpp
@@ -0,0 +1,94 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#define DEFAULT_PORT "27016"
+#define DEFAULT_BUFLEN 512
+
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+struct addrinfo* result = NULL, * ptr = NULL, hints;
+int iResult;
+WSADATA wsaData;
+
+int main()
+{
+	setlocale(0, "rus");
+	printf("Инициализация сокета...\n");
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+	printf("Создание сокета...\n");
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	iResult = getaddrinfo("172.20.10.8", DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ConnectSocket = INVALID_SOCKET;
+
+	ptr = result;
+
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+	}
+
+	const char* sendbuf = "this is a test";
+	int iResult;
+
+	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+	if (iResult == SOCKET_ERROR) {
+		printf("send failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	printf("Bytes Send: %ld\n", iResult);
+
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	char recvbuf[DEFAULT_BUFLEN];
+	do {
+		iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN-1, 0);
+		if (iResult > 0) {
+			recvbuf[iResult] = '\0';
+			printf("Message received: %s\n", recvbuf);
+		}
+		else if (iResult == 0)
+			printf("Connection closed\n");
+		else
+			printf("recv failed: %d\n", WSAGetLastError());
+	} while (iResult > 0);
+	closesocket(ConnectSocket);
+	WSACleanup();
+	return 0;
+}
---
+++ Prakt15Server/Prakt15Server/Prakt15Server.cpp
@@ -0,0 +1,105 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#define DEFAULT_PORT "27016"
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <conio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+struct addrinfo * result = NULL, * ptr = NULL, hints;
+int iResult;
+WSADATA wsaData;
+
+int main()
+{
+	setlocale(0, "rus");
+	printf("Инициализация сокета...\n");
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+	printf("Создание сокета...\n");
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+	printf("Привязка сокета...\n");
+
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+
+		closesocket(ListenSocket);
+		WSACleanup();
+
+		return 1;
+	}
+	freeaddrinfo(result);
+	printf("Сокет слушается!\n");
+
+	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+		printf("Listen failed: %ld\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	while (true) {
+		if (_getch()) {
+			break;
+		}
+		else {
+			continue;
+		}
+
+		SOCKET ClientSocket;
+
+		ClientSocket = accept(ListenSocket, NULL, NULL);
+		if (ClientSocket == INVALID_SOCKET) {
+			printf("accept failed: %d\n", WSAGetLastError());
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+		printf("Клиент подключен!\n");
+
+		const char* ping = "ping";
+
+		while (iResult = send(ClientSocket, ping, (int)strlen(ping), 0) != SOCKET_ERROR) {
+			cout << "Клиент подключен" << endl;
+			Sleep(1000);
+		}
+
+
+	}
+	closesocket(ListenSocket);
+	return 0;
+}
