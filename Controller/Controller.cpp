dc09b6230f3a7a45c71f1400d25b25ffe3c30ecf
ilonaSag <kupoles09@gmail.com>
2026-04-25 14:38:56 +0500

    init

---
+++ Controller/Controller.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+HANDLE hEvents[2];
+bool isWork = true;
+
+int main()
+{
+	setlocale(0, "rus");
+	int action;
+
+	hEvents[0] = OpenEvent(EVENT_ALL_ACCESS, NULL, L"EventOneExamB6");
+	hEvents[1] = OpenEvent(EVENT_ALL_ACCESS, NULL, L"EventTwoExamB6");
+
+	if (hEvents[0] == NULL) {
+		cout << GetLastError() << endl;
+		return 0;
+	}
+
+	if (hEvents[1] == NULL) {
+		cout << GetLastError() << endl;
+		return 0;
+	}
+
+	while (isWork) {
+		cout << "1 - Вывод счетчика" << endl;
+		cout << "2 - Стоп/Старт счетчика" << endl;
+		cout << "3 - Закрыть программу" << endl;
+
+		cin >> action;
+
+		switch (action) {
+		case 1:
+		{
+			SetEvent(hEvents[1]);
+			break;
+		}
+		case 2:
+		{
+			SetEvent(hEvents[0]);
+			break;
+		}
+		case 3: {
+			for (int i = 0; i < 2; i++)
+			{
+				CloseHandle(hEvents[i]);
+			}
+			isWork = false;
+			return 0;
+		}
+		}
+	}
+	return 0;
+
+}
---
+++ KuznetsovAA_03_04/KuznetsovAA_03_04.cpp
@@ -0,0 +1,94 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+HANDLE hEvents[2];
+
+int countEx = 0;
+
+HANDLE hThreads[3];
+
+DWORD idThreads[3];
+
+bool isPause = false;
+
+DWORD WINAPI counter(LPVOID) {
+	while (countEx < 100) {
+		countEx++;
+		Sleep(1000);
+	}
+	return 0;
+}
+
+DWORD WINAPI hThreadEventOne(LPVOID) {
+	while (countEx < 100) {
+		WaitForSingleObject(hEvents[0], INFINITE);
+		if (isPause) {
+			ResumeThread(hThreads[0]);
+			isPause = false;
+		}
+		else {
+			SuspendThread(hThreads[0]);
+			isPause = true;
+		}
+		ResetEvent(hEvents[0]);
+	}
+	return 0;
+}
+
+DWORD WINAPI hThreadEventTwo(LPVOID) {
+	while (countEx < 100) {
+		WaitForSingleObject(hEvents[1], INFINITE);
+		cout << countEx << endl;
+		ResetEvent(hEvents[1]);
+	}
+	return 0;
+}
+
+int main()
+{
+	hEvents[0] = CreateEvent(NULL, FALSE, FALSE, L"EventOneExamB6");
+	hEvents[1] = CreateEvent(NULL, FALSE, FALSE, L"EventTwoExamB6");
+
+	if (hEvents[0] == NULL) {
+		cout << GetLastError() << endl;
+		return 0;
+	}
+
+	if (hEvents[1] == NULL) {
+		cout << GetLastError() << endl;
+		return 0;
+	}
+
+	hThreads[0] = CreateThread(NULL, 0, counter, NULL, 0, &idThreads[0]);
+	hThreads[1] = CreateThread(NULL, 0, hThreadEventOne, NULL, 0, &idThreads[1]);
+	hThreads[2] = CreateThread(NULL, 0, hThreadEventTwo, NULL, 0, &idThreads[2]);
+
+	if (hThreads[0] == NULL) {
+		cout << GetLastError() << endl;
+		return 0;
+	}
+
+	if (hThreads[1] == NULL) {
+		cout << GetLastError() << endl;
+		return 0;
+	}
+
+	if (hThreads[2] == NULL) {
+		cout << GetLastError() << endl;
+		return 0;
+	}
+
+	WaitForMultipleObjects(3, hThreads, TRUE, INFINITE);
+
+	for (int i = 0; i < 3; i++) {
+		CloseHandle(hThreads[i]);
+	}
+
+	for (int i = 0; i < 2; i++) {
+		CloseHandle(hEvents[i]);
+	}
+	return 0;
+}
